71f031d3ff4f85ab0fd4bb032e5bf1daf4de70d4
Saif <droconos99@gmail.com>
2024-11-08 23:58:00 +0600

    Remove: Supabase config.
    Add: Homepage landing texts

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -2,6 +2,8 @@ import ProductGridSection from "@/components/ProductGridSection";
 import db from "@/db/db";
 import { cache } from "@/lib/cache";
 import { Product } from "@prisma/client";
+import { Computer, MonitorDown } from "lucide-react";
+import Image from "next/image";
 
 const getMostPopularProducts = cache(
    (): Promise<Product[]> => {
@@ -40,11 +42,27 @@ const getNewestProducts = cache(
 export default function HomePage(): React.ReactNode {
    return (
       <main className="space-y-12">
+         <LandingSection />
          <ProductGridSection
             title="Most popular"
             productsFetcher={getMostPopularProducts}
          />
-         <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+         <ProductGridSection title="Latest" productsFetcher={getNewestProducts} />
       </main>
+   );
+}
+
+function LandingSection() {
+   return (
+      <div className="">
+         <MonitorDown className="w-24 h-24 mx-auto" />
+         <h1 className="text-center text-3xl font-semibold">
+            Welcome to <span className="font-bold text-gray-700">Assets Haven!</span>
+         </h1>
+         <h2 className="text-center text-xl">
+            A platform to trade and download digital assets, softwares and many other
+            products with ease.
+         </h2>
+      </div>
    );
 }
